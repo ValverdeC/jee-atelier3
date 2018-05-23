cc7ce24ea2e5ab3ccebbfe5e22406ef83dca6f43
Corentin VALVERDE <corentin-externe.valverde@edf.fr>
2018-05-23 15:13:35 +0200

    ezlkjzelk

---
+++ MS_Salle/src/main/java/com/microservices/rooms/RoomApp.java
@@ -0,0 +1,11 @@
+package com.microservices.rooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomApp {
+	public static void main(String[] args) {
+		SpringApplication.run(RoomApp.class, args);
+	}
+}
