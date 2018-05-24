b6968b0f12e30e6f696b457ddd8507391187cc59
Julien DUCROT <julien.ducrot.mess@gmail.com>
2018-05-24 16:01:58 +0200

    Add auth and sign up

--- MS_Vue/src/main/java/com/sample/controller/UserRestController.java
+++ MS_Vue/src/main/java/com/sample/controller/UserRestController.java
@@ -1,26 +1,40 @@
 package com.sample.controller;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONString;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/users")
 public class UserRestController extends AbstracRestController{
 
     @RequestMapping(method = RequestMethod.POST, value = "/login", produces = "application/json")
-    private String login(@RequestBody String email, String password) {
-        //TODO
+    private String login(@RequestBody String body) {
 
-        ///auth/signup POST + JSON(name,email,pwd) => token
+        //JSONObject datas = this.parseRequestBody(body);
 
-        ///auth/login POST + JSON(email,mdp) => retourn token
+        //JSONString result = this.post("http://localhost:8090/auth/login", body.toString());
 
-        // /me GET + token
-
-        //@RequestHeader("Autorization")
-
+        //return result.toString();
         return null;
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/signup", produces = "application/json")
+    private String signup(@RequestBody JSONObject body) {
 
+        //JSONString result = this.post("http://localhost:8090/auth/signup", body.toString());
 
+        //return result.toString();
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/me", produces = "application/json")
+    private String me(@RequestHeader("Autorization") String auto) {
+
+        // /me GET + token
+        //@RequestHeader("Autorization")
+        return null;
+    }
 }
