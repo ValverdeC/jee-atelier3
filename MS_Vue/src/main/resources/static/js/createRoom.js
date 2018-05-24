e3c8fe96549f9660a5c1a4cfc32f1c431e8f5aaf
Julien DUCROT <julien.ducrot.mess@gmail.com>
2018-05-24 14:08:22 +0200

    rename project

--- MS_Vue/src/main/resources/static/js/createRoom.js
+++ MS_Vue/src/main/resources/static/js/createRoom.js
@@ -5,44 +5,10 @@ $(document ).ready(function(){
         //TO DO
     });  
     
-    /*$("#createButtonId").click(function() {
-    	var room = document.getElementById("uniqueID").value;
-    	
-    	alert(room);
-    	
-		/*$.ajax({
-			url: 'http://localhost:8080/api/game/createRoom/123',
-			type: 'POST',
-			contentType: "application/json; charset=utf-8",
-			dataType: "json",
-			data: json,
-			success: function(msg) {
-				alert('In Ajax');
-			}
-		});
-    });*/ 
+    $("#createButtonId").click(function(){
+        alert("Search button clicked :"+$("#roomBetId").val());
+        //TO DO
+    }); 
     
-    $("#roomForm").submit(function(e){
-    	e.preventDefault();
-    	
-    	var roomCreator = {
-    		estLancee: false,
-    		idJoueur1: 123,
-    		idCarteJoueur1: 34,
-    		nbTour: document.getElementById("roomHitId").value,
-			name: document.getElementById("roomNameId").value,
-	    	bet: document.getElementById("roomBetId").value
-		};
-    	
-    	$.ajax({
-			url: 'http://localhost:8080/api/game/createRoom',
-			type: 'POST',
-			contentType: "application/json; charset=utf-8",
-			dataType: "json",
-			data: JSON.stringify(roomCreator),
-			success: function(msg) {
-				alert('In Ajax');
-			}
-		});
-    });
+    
 });
