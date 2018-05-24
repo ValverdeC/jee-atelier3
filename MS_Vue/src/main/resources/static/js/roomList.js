e256ee786ddfa2753796fea0f1114f4951bc1650
Corentin VALVERDE <corentin-externe.valverde@edf.fr>
2018-05-24 14:03:31 +0200

    rename folder

---
+++ MS_Vue/src/main/resources/static/js/roomList.js
@@ -0,0 +1,53 @@
+$(document ).ready(function(){
+    
+   
+    
+    for(i=0;i<5;i++){
+        addRoomToList(i,"room "+ i,"user" +i, 500);
+    }
+    
+     $("#createRoomButtonId").click(function(){
+        alert("Create Card button clicked ");
+        //TO DO
+    }); 
+    
+});
+
+function getCards(){
+    var cards = [];
+
+    $.ajax({
+    		url: 'http://localhost:8080/api/cards/',
+    		type: 'GET',
+    		complete : function(resultat, statut){
+    			var cards = resultat;
+    			return cards;
+           	}
+    });
+}
+
+
+function addRoomToList(id,name, user, bet){
+    
+    content="<td> "+name+" </td> \
+                            <td> "+user+" </td> \
+                            <td> "+bet+" $</td> \
+                            <td> \
+                                <div class='center aligned'> \
+                                    <div class='ui  vertical animated button' tabindex='0' onClick='onRoomSelected("+id+")'> \
+                                        <div class='hidden content'>Play</div> \
+                                        <div class='visible content'> \
+                                            <i class='play circle icon'></i> \
+                                        </div> \
+                                    </div> \
+                                </div> \
+                            </td>";
+    
+    $('#roomListId tr:last').after('<tr>'+content+'</tr>');
+    
+    
+};
+
+function onRoomSelected(id){
+    alert("Room selected : " +id);
+}
