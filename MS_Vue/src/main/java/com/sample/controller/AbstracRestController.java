d9d4c00f4a6c1e1f2ffb32df070d06b69f73e3d9
Corentin VALVERDE <corentin-externe.valverde@edf.fr>
2018-05-24 17:23:17 +0200

    correct rebase

--- MS_Vue/src/main/java/com/sample/controller/AbstracRestController.java
+++ MS_Vue/src/main/java/com/sample/controller/AbstracRestController.java
@@ -1,33 +1,33 @@
 package com.sample.controller;
 
-import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
-import org.json.JSONString;
 
 public class AbstracRestController {
 
     RestTemplate restTemplate = new RestTemplate();
 
     public String get(String urlP){
-        String json = restTemplate.getForObject(urlP, String.class);
+    	String json = restTemplate.getForObject(urlP, String.class);
         return json;
     }
 
-    public String post(String urlP, String request){
-        String json = restTemplate.postForObject(urlP, request, String.class);
+    public String post(String urlP, String body) {
+    	HttpHeaders httpHeader = new HttpHeaders();
+    	httpHeader.setContentType(MediaType.APPLICATION_JSON);
+    	HttpEntity<String> entity = new HttpEntity<String>(body, httpHeader);
+    	
+    	String json = restTemplate.postForObject(urlP, entity, String.class);
         return json;
     }
 
-    public JSONString put(String urlP, String request){
+    public String put(String urlP, String request){
         return null;
     }
 
-    public JSONString delete(String urlP){
+    public String delete(String urlP){
         return null;
-    }
-
-    public boolean isUserConnected(){
-        //TODO
-        return true;
     }
 }
--- MS_Vue/src/main/resources/static/js/createRoom.js
+++ MS_Vue/src/main/resources/static/js/createRoom.js
@@ -5,10 +5,44 @@ $(document ).ready(function(){
         //TO DO
     });  
     
-    $("#createButtonId").click(function(){
-        alert("Search button clicked :"+$("#roomBetId").val());
-        //TO DO
-    }); 
+    /*$("#createButtonId").click(function() {
+    	var room = document.getElementById("uniqueID").value;
+    	
+    	alert(room);
+    	
+		/*$.ajax({
+			url: 'http://localhost:8080/api/game/createRoom/123',
+			type: 'POST',
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			data: json,
+			success: function(msg) {
+				alert('In Ajax');
+			}
+		});
+    });*/ 
     
-    
+    $("#roomForm").submit(function(e){
+    	e.preventDefault();
+    	
+    	var roomCreator = {
+    		estLancee: false,
+    		idJoueur1: 123,
+    		idCarteJoueur1: 34,
+    		nbTour: document.getElementById("roomHitId").value,
+			name: document.getElementById("roomNameId").value,
+	    	bet: document.getElementById("roomBetId").value
+		};
+    	
+    	$.ajax({
+			url: 'http://localhost:8080/api/game/createRoom',
+			type: 'POST',
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			data: JSON.stringify(roomCreator),
+			success: function(msg) {
+				alert('In Ajax');
+			}
+		});
+    });
 });
