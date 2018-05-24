1acfee40594437bf196845e751ae7af64ceab898
Corentin VALVERDE <corentin-externe.valverde@edf.fr>
2018-05-24 17:23:17 +0200

    correct rebase

--- MS_Vue/src/main/java/com/sample/controller/GameRestController.java
+++ MS_Vue/src/main/java/com/sample/controller/GameRestController.java
@@ -1,17 +1,23 @@
 package com.sample.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.sample.model.Room;
 
 @RestController
 @RequestMapping("/api/game")
 public class GameRestController extends AbstracRestController{
 
-    @RequestMapping(value = "createRoom/{userId}", method = RequestMethod.POST, produces = "application/json")
-    public String createRoom(@PathVariable int userId){
-        //TODO
+    @RequestMapping(value = "createRoom", method = RequestMethod.POST, produces = "application/json")
+    public String createRoom(@RequestBody Room roomCreator) {
+    	Gson gson = new Gson();
+        this.post("http://localhost:4200/game/room", gson.toJson(roomCreator));
         return null;
     }
 
@@ -19,5 +25,13 @@ public class GameRestController extends AbstracRestController{
     public String joinRoom(@PathVariable int roomId){
         //TODO
         return null;
+    }
+    
+    @GetMapping("/rooms")
+    public Room[] getRooms() throws Exception {
+    	String roomsString = this.get("http://localhost:4200/game/rooms");
+    	Gson gson = new Gson();
+    	Room rooms[] = gson.fromJson(roomsString, Room[].class);
+    	return rooms;
     }
 }
