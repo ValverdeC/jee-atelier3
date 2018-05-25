32dbf7a25600622829f58e8f1e394383cec7bcf1
Julien DUCROT <julien.ducrot.mess@gmail.com>
2018-05-25 14:57:54 +0200

    card market

---
+++ MS_Vue/bin/src/main/resources/static/js/roomList.js
@@ -0,0 +1,55 @@
+$(document ).ready(function(){
+	
+	getCards();
+        
+     $("#createRoomButtonId").click(function(){
+    	 window.location = "/create";
+    }); 
+    
+});
+
+function getCards(){
+    var cards = [];
+
+    $.ajax({
+    		url: 'http://localhost:8080/api/game/rooms',
+    		type: 'GET',
+    		complete : function(resultat, statut) {
+    			var cards = resultat.responseJSON;
+    			
+    			if (cards != null) {
+    				cards.forEach((card) => {
+    					addRoomToList(card.id, card.name, card.idJoueur1, card.bet);
+    				})
+    			}
+    			
+    			return cards;
+           	}
+    });
+}
+
+
+function addRoomToList(id, name, user, bet){
+    
+    content="<td> "+name+" </td> \
+                            <td> "+user+" </td> \
+                            <td> "+bet+" $</td> \
+                            <td> \
+                                <div class='center aligned'> \
+                                    <div class='ui  vertical animated button' tabindex='0' onClick='onRoomSelected("+id+")'> \
+                                        <div class='hidden content'>Play</div> \
+                                        <div class='visible content'> \
+                                            <i class='play circle icon'></i> \
+                                        </div> \
+                                    </div> \
+                                </div> \
+                            </td>";
+    
+    $('#roomListId tr:last').after('<tr>'+content+'</tr>');
+    
+    
+};
+
+function onRoomSelected(id){
+    alert("Room selected : " +id);
+}
