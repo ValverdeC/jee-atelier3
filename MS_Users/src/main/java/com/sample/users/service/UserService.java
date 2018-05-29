7813329ecce8e123963153408b7d8fe7a3be37b8
Ilias Redissi <ilias.redissi@gmail.com>
2018-05-29 20:59:15 +0200

    Ajout de la documentation pour le microservice Utilisateur

--- MS_Users/src/main/java/com/sample/users/service/UserService.java
+++ MS_Users/src/main/java/com/sample/users/service/UserService.java
@@ -19,6 +19,13 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Récupération d'un utilisateur selon son identifiant
+     *
+     * @param id L'identifiant de l'utilisateur
+     * @return L'utilisateur sélectionné
+     * @throws UserNotFoundException Si l'utilisateur n'a pas été trouvé
+     */
     public User getUser(String id) throws UserNotFoundException {
         User user = userRepository.findOne(Integer.valueOf(id));
         if (user == null) {
@@ -27,6 +34,13 @@ public class UserService {
         return user;
     }
 
+    /**
+     * Inscription d'un utilisateur
+     * @param user L'utilisateur qui s'inscrit
+     * @return Le token de l'utilisateur
+     * @throws EmailAlreadyUsedException Si un utilisateur existe déjà avec l'adresse mail renseigné
+     * @throws ParameterNotSpecifiedException Si l'adresse mai, le nom ou le mot de passe sont pas fournis
+     */
     public String addUser(User user) throws EmailAlreadyUsedException, ParameterNotSpecifiedException {
         if (user.getPassword() == null || user.getPassword().isEmpty()) {
             throw new PasswordNotSpecifiedException();
@@ -46,6 +60,14 @@ public class UserService {
         return user.getToken();
     }
 
+    /**
+     * Récupération d'un utilisateur
+     * @param email L'adresse mail de l'utilisateur.
+     * @param password Le mot de l'utilisateur.
+     * @return L'utilisateur sélectionné
+     * @throws UserNotFoundException Si l'utilisateur n'a pas été trouvé.
+     * @throws ParameterNotSpecifiedException Si l'adresse mail ou le mot de passe sont pas fournis.
+     */
     public User getUserByEmailAndPassword(String email, String password) throws UserNotFoundException, ParameterNotSpecifiedException {
         if (password == null || password.isEmpty()) {
             throw new PasswordNotSpecifiedException();
@@ -61,6 +83,12 @@ public class UserService {
         return user;
     }
 
+    /**
+     * Récupération d'un utilisateur selon son token
+     * @param token Le token de l'utilisateur
+     * @return L'utilisateur sélectionné
+     * @throws UserNotFoundException Si l'utilisateur n'a pas été trouvé
+     */
     public User getUserByToken(String token) throws UserNotFoundException {
         User user = userRepository.findByToken(token);
         if (user == null) {
@@ -69,6 +97,13 @@ public class UserService {
         return user;
     }
 
+    /**
+     * Ajoute de l'argent au porte-monnaie de l'utilisateur
+     * @param token Le token de l'utilisateur
+     * @param amount Montant à ajouter
+     * @return Le nouveau montant du porte-monnaie
+     * @throws UserNotFoundException Si l'utilisateur n'a pas été trouvé
+     */
     public BigDecimal addToWallet(String token, BigDecimal amount) throws UserNotFoundException {
         User user = this.getUserByToken(token);
         user.addToWallet(amount);
@@ -76,6 +111,13 @@ public class UserService {
         return user.getWallet();
     }
 
+    /**
+     * Enlève de l'argent au porte-monnaie de l'utilisateur
+     * @param token Le token de l'utilisateur
+     * @param amount Montant à enlever
+     * @return Le nouveau montant du porte-monnaie
+     * @throws UserNotFoundException Si l'utilisateur n'a pas été trouvé
+     */
     public BigDecimal removeFromWallet(String token, BigDecimal amount) throws UserNotFoundException {
         User user = this.getUserByToken(token);
         user.removeFromWallet(amount);
