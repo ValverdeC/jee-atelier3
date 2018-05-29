a553f894e2358665e25384c7ac4f2ba95198819f
Ilias Redissi <ilias.redissi@gmail.com>
2018-05-29 20:59:15 +0200

    Ajout de la documentation pour le microservice Utilisateur

--- MS_Users/src/main/java/com/sample/users/controller/UserController.java
+++ MS_Users/src/main/java/com/sample/users/controller/UserController.java
@@ -21,6 +21,14 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * Connexion d'un utilisateur
+     *
+     * @param json Un JSON avec l'adresse mail et le mot de passe de l'utilisateur
+     * @return Le token de l'utilisateur
+     * @throws UserNotFoundException          Si l'utilisateur n'a pas été trouvé
+     * @throws ParameterNotSpecifiedException Si l'adresse mail ou le mot de passe sont pas fournis
+     */
     @RequestMapping(method = RequestMethod.POST, value = "/auth/login")
     private String login(@RequestBody Map<String, String> json) throws UserNotFoundException, ParameterNotSpecifiedException {
         String email = json.get("email");
@@ -30,32 +38,71 @@ public class UserController {
         return user.getToken();
     }
 
+    /**
+     * Inscription d'un utilisateur
+     * @param user L'utilisateur qui s'inscrit
+     * @return Le token de l'utilisateur
+     * @throws EmailAlreadyUsedException Si un utilisateur existe déjà avec l'adresse mail renseigné
+     * @throws ParameterNotSpecifiedException Si l'adresse mai, le nom ou le mot de passe sont pas fournis
+     */
     @RequestMapping(method = RequestMethod.POST, value = "/auth/signup")
     private String signup(@RequestBody User user) throws EmailAlreadyUsedException, ParameterNotSpecifiedException {
         return userService.addUser(user);
     }
 
+    /**
+     * Récupération d'un utilisateur selon son identifiant
+     * @param userid L'identifiant de l'utilisateur
+     * @return Les informations de l'utilisateur
+     * @throws UserNotFoundException Si l'utilisateur n'a pas été trouvé
+     */
     @RequestMapping("/users/{userid}")
     private User getUser(@PathVariable String userid) throws UserNotFoundException {
         return userService.getUser(userid);
     }
 
+    /**
+     * Récupération d'un utilisateur selon son token
+     * @param token Le token de l'utilisateur
+     * @return Les informations de l'utilisateur
+     * @throws UserNotFoundException Si l'utilisateur n'a pas été trouvé
+     */
     @RequestMapping("/me")
     private User getUserByToken(@RequestHeader("Authorization") String token) throws UserNotFoundException {
         return userService.getUserByToken(token);
     }
 
+    /**
+     * Récupération du montant du porte-monnaie de l'utilisateur
+     * @param token Le token de l'utilisateur
+     * @return Le montant du porte-monnaie
+     * @throws UserNotFoundException Si l'utilisateur n'a pas été trouvé
+     */
     @RequestMapping("/me/wallet")
     private BigDecimal getWallet(@RequestHeader("Authorization") String token) throws UserNotFoundException {
         User user = userService.getUserByToken(token);
         return user.getWallet();
     }
 
+    /**
+     * Ajoute de l'argent au porte-monnaie de l'utilisateur
+     * @param token Le token de l'utilisateur
+     * @param amount Montant à ajouter
+     * @return Le nouveau montant du porte-monnaie
+     * @throws UserNotFoundException Si l'utilisateur n'a pas été trouvé
+     */
     @RequestMapping("/me/wallet/add/{amount:.+}")
     private BigDecimal addToWallet(@RequestHeader("Authorization") String token, @PathVariable BigDecimal amount) throws UserNotFoundException {
         return userService.addToWallet(token, amount);
     }
 
+    /**
+     * Enlève de l'argent au porte-monnaie de l'utilisateur
+     * @param token Le token de l'utilisateur
+     * @param amount Montant à enlever
+     * @return Le nouveau montant du porte-monnaie
+     * @throws UserNotFoundException Si l'utilisateur n'a pas été trouvé
+     */
     @RequestMapping("/me/wallet/remove/{amount:.+}")
     private BigDecimal removeFromWallet(@RequestHeader("Authorization") String token, @PathVariable BigDecimal amount) throws UserNotFoundException {
         return userService.removeFromWallet(token, amount);
